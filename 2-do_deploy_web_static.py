7d8b0e29c457129daa63ec128531721aa08308e7
LYCE899 <a.coulibaly@alustudent.com>
2023-06-22 14:14:46 +0000

    setup,pack,deploy,do_deploy (web)

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+# Fabfile to distribute an archive to your web servers,
+
+from fabric.api import env, put, run, local
+from os.path import exists, isdir
+import os.path
+import re
+
+
+# Set the username and host for SSH connection to the server
+env.user = 'ubuntu'
+env.hosts = ['107.20.75.201', '54.235.37.236']
+env.key_filename = '~/.ssh/id_rsa'
+
+
+def do_deploy(archive_path):
+    """deploy web static"""
+    if not exists(archive_path):
+        return False
+    try:
+        file_name = archive_path.split("/")[-1]
+        name = file_name.split(".")[0]
+        path_name = "/data/web_static/releases/" + name
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}/".format(path_name))
+        run('tar -xzf /tmp/{} -C {}/'.format(file_name, path_name))
+        run("rm /tmp/{}".format(file_name))
+        run("mv {}/web_static/* {}".format(path_name, path_name))
+        run("rm -rf {}/web_static".format(path_name))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}/ /data/web_static/current'.format(path_name))
+        return True
+    except Exception:
+        return False
